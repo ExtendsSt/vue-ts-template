4cb6896ede1daf8848c32ad66edd185119cd4549
Extends <1281826717@qq.com>
2024-01-31 11:07:23 +0800

    add 版权所有

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,6 +4,11 @@
 
 <template>
   <div>
-    <button>Theme</button>
+    <div class="h-screen w-screen flex-center flex-col text-center">
+      <div>开发中</div>
+      <div>
+        © 2023-2024 laogewen.cn 版权所有 <a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备2024044706号-1</a>
+      </div>
+    </div>
   </div>
 </template>
