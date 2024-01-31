b301fe7c57227fcfb432acde7c63c2041392daac
Extends <1281826717@qq.com>
2024-01-31 11:23:50 +0800

    测试打包

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@
 <template>
   <div>
     <div class="h-screen w-screen flex-center flex-col text-center">
-      <div>开发中</div>
+      <div>开发中 测试打包</div>
       <div>
         © 2023-2024 laogewen.cn 版权所有 <a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备2024044706号-1</a>
       </div>
